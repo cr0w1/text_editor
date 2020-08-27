8351a83de5a558467c47c6d883121c5954b3ed03
cr0w1 <clebsonjose30@gmail.com>
2020-08-27 15:48:31 -0300

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About(){
+
+    return (
+        <div style={{
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <p>Bem vindo a pagina Home</p>
+        </div>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Routes,
+    Route
+ } from 'react-router-dom';
+
+import Home from './pages/home/index';
+import About from './pages/about/index';
+
+ export default function MainRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    )
+ }
